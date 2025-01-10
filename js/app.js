e8aaab6b3de6235c496e3ad7e8b2c6ecb7c3b90c
abdoemadselim <abdoemadselim11@gmail.com>
2025-01-10 13:52:33 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/sw.js")
+    navigator.serviceWorker.register("../sw.js")
         .then(reg => console.log("Service worker registered successfully", reg))  // reg of serviceWorkerRegistration interface
         .catch(error => console.log("Service worker isn't registered", error))
 }
